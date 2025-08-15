59e1dfb855101960acab5353cf4d2b19df207377
paul <drazen90sea@gmail.com>
2025-08-15 11:10:48 +0100

    commit

--- Operators.java
+++ Operators.java
@@ -1,6 +1,10 @@
 public class Operators {
    public static void main(String[] args) {
-    
+    /*
+     * arithimetic operators
+     * logical operators
+     * relational operators
+     */
    }
     
 }
