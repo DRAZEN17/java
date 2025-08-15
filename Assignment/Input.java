5c28cd9c7f656fd7a26c8d46022358b99ac195f3
paul <drazen90sea@gmail.com>
2025-08-15 12:17:45 +0100

    commit

---
+++ Assignment/Input.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("wats ur age : ");
+         int age = scanner.nextInt();
+         System.out.println("ur " + age +"yrs old");
+    }
+}
