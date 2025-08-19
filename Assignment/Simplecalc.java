7430c9570ce2f6d3fa45e18ea93b8ed969be522a
paul <drazen90sea@gmail.com>
2025-08-19 21:35:16 +0100

    commit

--- Assignment/Simplecalc.java
+++ Assignment/Simplecalc.java
@@ -1,5 +1,40 @@
 package Assignment;
+import java.util.Scanner;
 
 public class Simplecalc {
-    
+  public static void main(String[] args) {
+    Scanner newscanner = new Scanner(System.in);
+
+        System.out.print("What is your first number: ");
+        float num1 = newscanner.nextFloat();
+
+        System.out.print("What is your second number: ");
+        float num2 = newscanner.nextFloat();
+
+        System.out.print("Choose an operation (sum, difference, product, quotient): ");
+        String op = newscanner.next();
+
+        if (op.equalsIgnoreCase("sum")) {
+            float sum = num1 + num2;
+            System.out.println("The sum is: " + sum);
+        } else if (op.equalsIgnoreCase("difference")) {
+            float difference = num1 - num2;
+            System.out.println("The difference is: " + difference);
+        } else if (op.equalsIgnoreCase("product")) {
+            float product = num1 * num2;
+            System.out.println("The product is: " + product);
+        } else if (op.equalsIgnoreCase("quotient")) {
+            if (num2 != 0) {
+                float quotient = num1 / num2;
+                System.out.println("The quotient is: " + quotient);
+            } else {
+                System.out.println("Error: Division by zero!");
+            }
+        } else {
+            System.out.println("Invalid operation. Please try again.");
+        }
+
+        newscanner.close();
+  }
+
 }
--- Control.java
+++ Control.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Control {
-    
+    public static void main(String[] args) {
+       Scanner newscanner = new Scanner(System.in);
+         System.out.println("wats ur height");
+       float height = newscanner.nextFloat();
+       if(height < 6.0){
+         System.out.println("ur tall");
+       }
+       else if(height > 3.5){
+        System.out.println("ur average");
+       }
+       else{
+        System.out.println("ur short");
+       }
+    }
 }
