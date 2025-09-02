a8db95a291e330c6acfdde61fa4fe4875ce26eef
paul <drazen90sea@gmail.com>
2025-09-02 20:00:20 +0100

    commit

--- Arrarys.java
+++ Arrarys.java
@@ -2,22 +2,34 @@ public class Arrarys {
     public static void main(String[] args) {
         String firstname = " west";
         String lname ="east";
-        String[] fulname = {"west","east"};
+        String ename ="south";
+        String jname ="north";
+        String[] fulname = {"west","east","south","north"};
 
         //coins
         //age
         /*
          * coinrperrun
          */
-        int coins = 100000;
-        int age = 13;
-        int coinperrun = 100;
+        // int coins = 100000;
+        // int age = 13;
+        // int coinperrun = 100;
          
-        int [] cointotl = {coins,age,coinperrun};
+        // int [] cointotl = {coins,age,coinperrun};
 
+        // int ages[] = {20,50,70,90};
 
-        System.out.print(cointotl[1]);
-        System.out.print(fulname[0]);
-        
+        //    int  sum =0;
+        //    for (int i = 0; i < ages.length; i++){
+        //       sum = sum + ages[i];
+        //    }
+
+        // System.out.print(cointotl[1]);
+        // System.out.print(fulname[0 ]);
+
+        // String [] directions = {"west","east","south","north"};
+        //   for ( int i = 0 ; i < directions.length; i++){
+        //           System.out.println(i);
+        //   }
     }
 }
