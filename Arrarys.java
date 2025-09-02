abc86f89ccd6fb3105156eca8643689628b1726a
paul <drazen90sea@gmail.com>
2025-09-02 15:51:07 +0100

    commit

---
+++ Arrarys.java
@@ -0,0 +1,23 @@
+public class Arrarys {
+    public static void main(String[] args) {
+        String firstname = " west";
+        String lname ="east";
+        String[] fulname = {"west","east"};
+
+        //coins
+        //age
+        /*
+         * coinrperrun
+         */
+        int coins = 100000;
+        int age = 13;
+        int coinperrun = 100;
+         
+        int [] cointotl = {coins,age,coinperrun};
+
+
+        System.out.print(cointotl[1]);
+        System.out.print(fulname[0]);
+        
+    }
+}
--- Loops.java
+++ Loops.java
@@ -1,18 +1,48 @@
+
 public class Loops {
+
     public static void main(String[] args) {
-      int start =  1;
+        // int start =  1;
 
-      int end = 50;
+        // int end = 50;
+        // while (start <= end) { 
+        //     if(start % 3 == 0 &&  start % 5 == 0){
+        //       System.out.println( start + " fizzbuzz");
+        //     }else if (start % 3 == 0){
+        //         System.out.println( start + " fizz");
+        //     }else if (start % 5 == 0){
+        //         System.out.println( start + " buzz");
+        //     }
+        //     start = start + 1 ;
+        // }
+        // do/while loop
+        // int i = 0;
 
-      while (start <= end) { 
-          if(start % 3 == 0 &&  start % 5 == 0){
-            System.out.println( start + " fizzbuzz");
-          }else if (start % 3 == 0){
-              System.out.println( start + " fizz");
-          }else if (start % 5 == 0){
-              System.out.println( start + " buzz");
+        // do {
+        //     System.out.println("this is the value of " + i);
+        //     i++;
+        // } while (i < 5);
+
+        // while (i < 5);
+        // {
+        //     System.out.println("this is the value of " + i);
+        //     i++;
+        // }
+        // int a = 3;
+        // int b = 1 ;
+        // while ( b <= 12 ){
+        //   System.out.println(a*b);
+        //   b++;
+
+        // }
+          
+        for ( int b = 0 ; b <= 50; b++){
+          if(b % 5 == 0 ){
+          System.out.println(b);
           }
-          start = start + 1 ;
-      }
+          else{
+            continue;
+          }
+        } 
     }
 }
